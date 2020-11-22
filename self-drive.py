92508dc0beaff78a6a52daf3e336b35efab86674
Job Henandez Lara <69167740+Jobhdez@users.noreply.github.com>
2020-11-22 11:32:18 -0800

    redefined functions

--- self-drive.py
+++ self-drive.py
@@ -1,15 +1,17 @@
 import scipy
 from itertools import islice
-from os import listdir
 import os
+import keras
 from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Dense
 from keras.layers import Flatten
-from keras.optimizers import SGD
 from keras.applications.vgg16 import VGG16
 from keras.models import Model
+from keras.models import load_model
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 import numpy as np
 import cv2
 
@@ -22,8 +24,9 @@ def train_and_save_model(train_x, train_y, test_x, test_y):
     model.fit(train_x, train_y, epochs=100, batch_size=64,
               validation_data=(test_x, test_y), verbose=0)
 
-    print("saving model")
-    model.save('autodrive.h5')
+    print("\nSaving Model\n")
+    model.save('autodrive2.h5')
+
 
 
 def make_model(in_shape=[256, 455, 3], out_shape=1):
@@ -44,6 +47,7 @@ def make_model(in_shape=[256, 455, 3], out_shape=1):
 
     return model
 
+
 def get_datasets(images, angles):
     """given a numpy array of images and a numpy array of angles make
 training and testing dataset"""
@@ -58,28 +62,37 @@ training and testing dataset"""
 
     return np.array(train_x), np.array(train_y), np.array(test_x), np.array(test_y)
 
-def make_image_list(folder, file_name):
-    """given a string denoting a  folder and a string denoting a file_name make a list
-of image paths(eg "folder/path.jpg")"""
-    images = []
-    with open(file_name) as fp:
-        for line in islice(fp, None):
-            path = line.strip().split()[0]
-            image_path = os.path.join(folder, path)
-            images.append(image_path)
-    
-    return images
-print(len(make_image_list(folder, file_name)))
-def make_angles_numpy(folder, file_name):
-    """given a string denoted as folder and a string denoted as file_name make a numpy
-array of list of angles"""
-    angles = []
-    with open(file_name) as fp:
-        for line in islice(fp, None):
-            angle = line.strip().split()[1].split(",")[0]
-            angles.append(float(angle)*scipy.pi/180)
-            
-    return np.array(angles)
+
+def make_list(folder, file_name, function):
+    """ consumes a string denoted as "folder", a string denoted as "file_name" and a function;
+the function is applied to each line of the textfile"""
+
+    return [function(line) for line in islice(open(file_name), None)]
+
+def make_image_lst(folder, file_name):
+    """ uses "make_list" to make a list of image paths"""
+
+    def operate_on_image(line):
+        """ given a line of text it extracts the image-name and then joins the string "folder" with the image-name"""
+        path = line.strip().split()[0] 
+        image_path = os.path.join(folder, path)
+        
+        return image_path
+
+    return make_list(folder, file_name, operate_on_image)
+
+def make_numpy_of_angles(folder, file_name):
+    """ given a folder(string) and a file_name(string) it makes a numpy array of angles"""
+
+    def operate_on_angle(line):
+        """ given a line of text it extracts one column(ie the angle) and assigns it to "angle" """
+        angle = line.strip().split()[1].split(",")[0]
+        angle = float(angle) * scipy.pi/180
+
+        return angle
+
+    return np.array(make_list(folder, file_name, operate_on_angle))
+        
 
 def preprocess_images(list_of_images):
     """given a list consisting of image paths return a numpy array of preprocessed images"""
@@ -96,14 +109,24 @@ file_name = os.path.join(folder, 'data2.txt')
                     
 # here, get_datasets consumes two functions: "preprocess_images" and "make_angle_arrays"; the former
 # retuns a numpy array of a list of preprocessed images and the latter returns and a numpy array of angles
-train_x, train_y, test_x, test_y = get_datasets(preprocess_images(make_image_list(folder, file_name)),
-                                                make_angles_numpy(folder, file_name))
+train_x, train_y, test_x, test_y = get_datasets(preprocess_images
+                                                (make_image_lst(folder, file_name)),
+                                                make_numpy_of_angles(folder, file_name))
 # run the model and save it
 train_and_save_model(train_x,
                      train_y,
                      test_x,
                      test_y)
-    
+                    
+            
+model = load_model('autodrive2.h5')
+predict_training = model.predict(train_x)
+print("\nTraining RMSE\n")
+print(np.sqrt(mean_squared_error(y_train, predict_training)))
+print("\nTesting RMSE\n")
+predict_testing = model.predict(test_x)
+print(np.sqrt(mean_squared_error(test_y, predict_testing)))
+
 
 
     
